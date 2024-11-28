f14a2a17b6d8f6c0a8a9fa7074b021d4e517d8a8
Kilian Bartz <s4kibart@uni-trier.de>
2024-11-28 14:15:02 +0100

    new cs_pipe ver

--- Aufgabe3/cs_pipe.c
+++ Aufgabe3/cs_pipe.c
@@ -1,107 +1,76 @@
-#define _GNU_SOURCE
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <sys/time.h>
 #include <unistd.h>
-#include <sched.h>
-#include <sys/wait.h>
-#include <sys/resource.h>
-#include <time.h>
-#include <sys/types.h>
-#include <x86intrin.h>
-
-int main()
+uint32_t COUNTER;
+pthread_mutex_t LOCK;
+pthread_mutex_t START;
+pthread_cond_t CONDITION;
+void *threads(
+    void *unused)
 {
-    int pipe1[2], pipe2[2];
-    pid_t pid;
-    char byte = 'A';
-    struct sched_param param;
-    cpu_set_t set;
-
-    // Create pipes
-    if (pipe(pipe1) == -1 || pipe(pipe2) == -1)
+    // Wait till we may fire away
+    pthread_mutex_lock(&START);
+    pthread_mutex_unlock(&START);
+    pthread_mutex_lock(&LOCK);
+    // If I'm not the first thread, the other thread is already waiting on
+    // the condition, thus Ihave to wake it up first, otherwise we'll deadlock
+    if (COUNTER > 0)
     {
-        perror("pipe");
-        exit(EXIT_FAILURE);
+        pthread_cond_signal(&CONDITION);
     }
-
-    // Set CPU affinity to CPU 0
-    CPU_ZERO(&set);
-    CPU_SET(0, &set);
-    if (sched_setaffinity(0, sizeof(set), &set) == -1)
+    for (;;)
     {
-        perror("sched_setaffinity");
-        exit(EXIT_FAILURE);
+        COUNTER++;
+        pthread_cond_wait(&CONDITION, &LOCK);
+        // Always wake up the other thread before processing. The other
+        // thread will not be able to do anything as long as I don't go
+        // back to sleep first.
+        pthread_cond_signal(&CONDITION);
     }
-
-    // Set real-time priority
-    param.sched_priority = sched_get_priority_max(SCHED_FIFO);
-    if (sched_setscheduler(0, SCHED_FIFO, &param) == -1)
-    {
-        perror("sched_setscheduler");
-        exit(EXIT_FAILURE);
-    }
-
-    pid = fork();
-    if (pid == -1)
-    {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
-
-    if (pid == 0)
-    { // Child process
-        unsigned long long start, end;
-        char recv_byte;
-
-        // Set same CPU affinity and priority for child
-        if (sched_setaffinity(0, sizeof(set), &set) == -1)
-        {
-            perror("child sched_setaffinity");
-            exit(EXIT_FAILURE);
-        }
-        if (sched_setscheduler(0, SCHED_FIFO, &param) == -1)
-        {
-            perror("child sched_setscheduler");
-            exit(EXIT_FAILURE);
-        }
-
-        for (int i = 0; i < 1000; i++)
-        {
-            // Wait for parent's signal
-            read(pipe1[0], &recv_byte, 1);
-
-            // Send back to parent
-            write(pipe2[1], &byte, 1);
-        }
-        exit(EXIT_SUCCESS);
-    }
-    else
-    { // Parent process
-        unsigned long long start, end;
-        char recv_byte;
-        double total_cycles = 0;
-        const int iterations = 10000;
-
-        for (int i = 0; i < iterations; i++)
-        {
-            start = __rdtsc();
-
-            // Signal child
-            write(pipe1[1], &byte, 1);
-
-            // Wait for child's response
-            read(pipe2[0], &recv_byte, 1);
-
-            end = __rdtsc();
-            total_cycles += (end - start) / 2.0; // Divide by 2 for one-way latency
-        }
-
-        // Wait for child to finish
-        wait(NULL);
-
-        printf("Average context switch latency: %.2f CPU cycles\n",
-               total_cycles / iterations);
-    }
-
+    pthread_mutex_unlock(&LOCK); // To unlock
+}
+int64_t timeInMS()
+{
+    struct timeval t;
+    gettimeofday(&t, NULL);
+    return (
+        (int64_t)t.tv_sec * 1000 +
+        (int64_t)t.tv_usec / 1000);
+}
+int main(
+    int argc,
+    char **argv)
+{
+    int64_t start;
+    pthread_t t1;
+    pthread_t t2;
+    int64_t myTime;
+    pthread_mutex_init(&LOCK, NULL);
+    pthread_mutex_init(&START, NULL);
+    pthread_cond_init(&CONDITION, NULL);
+    pthread_mutex_lock(&START);
+    COUNTER = 0;
+    pthread_create(&t1, NULL, threads, NULL);
+    pthread_create(&t2, NULL, threads, NULL);
+    pthread_detach(t1);
+    pthread_detach(t2);
+    // Get start time and fire away
+    myTime = timeInMS();
+    pthread_mutex_unlock(&START);
+    // Wait for about a second
+    sleep(10);
+    // Stop both threads
+    pthread_mutex_lock(&LOCK);
+    // Find out how much time has really passed. sleep won't guarantee me that
+    // I sleep exactly one second, I might sleep longer since even after being
+    // woken up, it can take some time before I gain back CPU time. Further
+    // some more time might have passed before I obtained the lock!
+    myTime = timeInMS() - myTime;
+    // Correct the number of thread switches accordingly
+    COUNTER = (uint32_t)(((uint64_t)COUNTER * 1000) / myTime);
+    printf("Number of thread switches in about one second was %u\n", COUNTER);
     return 0;
 }
